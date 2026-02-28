3f134932d9d49f72d6a3a0fe3833c38471ef587a
vkhemkaasc <vaibhaw.khemka@ascendion.com>
2026-02-28 20:59:21 +0530

    env changes will work for both ec2 and local

--- src/memu/database/postgres/migrations/env.py
+++ src/memu/database/postgres/migrations/env.py
@@ -42,7 +42,8 @@ def include_object(object: Any, name: str, type_: str, reflected: bool, compare_
 
 
 def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
+    import os 
+    url = os.environ.get("DATABASE_URL") or config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
